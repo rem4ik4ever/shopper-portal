6b815d2243b1b7daacd6bc838b49ef5c3130644d
Rem Kim <kim.rmi.93@gmail.com>
2022-01-20 13:14:54 -0500

    initial commit
    Signed-off-by: Rem Kim <kim.rmi.93@gmail.com>

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "@components/common"
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        Main container
+      </div>
+    </div>
+  )
+}
+
+Home.Layout = Layout
